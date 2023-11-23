265261e2f20f63e873f4af1afdacc0c898e86b3c
Matus Yaowvasrisuwan <matus.y@kkumail.com>
2023-11-23 11:50:45 +0700

    Initialize Github Repo

---
+++ src/main/java/net/wasabo/tutorialmod/TutorialModCilent.java
@@ -0,0 +1,11 @@
+package net.wasabo.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModCilent implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
